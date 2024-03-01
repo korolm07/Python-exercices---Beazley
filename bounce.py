f8aeabc1f163430048e3ec04177344fb547f7ae8
korolm07 <59730491+korolm07@users.noreply.github.com>
2024-03-01 15:14:03 +0100

    Update bounce.py

--- bounce.py
+++ bounce.py
@@ -1,3 +1,11 @@
 git remote add origin https://github.com/korolm07/Python-exercices---Beazley.git
 git branch -M main
 git push -u origin main
+bounce = 3/5
+height_start = 100
+height = height_start*bounce
+number = 1
+while number < 11:
+  print(number,round(height,4))
+  number = number + 1
+  height = height * bounce
